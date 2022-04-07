f6ddbd77124e26646f8d1c9f58ad22d1d4ee4d7b
QuackDuster <jjimenez3@ucol.mx>
2022-04-06 22:22:38 -0500

    Simple Form validation using DataTriggers

---
+++ FormValidation/ViewModel/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FormValidation.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _age;
+        private bool _isValid;
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; Validate(); }
+        }
+
+        public string Age
+        {
+            get => _age;
+            set { _age = value; Validate(); }
+        }
+
+        public bool IsValid 
+        { 
+          get => _isValid; 
+          set
+          {
+              _isValid = value;
+              OnPropertyChanged();
+          }
+        }
+
+        public void Validate()
+        {
+            if (!string.IsNullOrEmpty(Name) && Name.Length < 20 )
+            {
+                IsValid = true;
+            }
+            else
+            {
+                IsValid = false;
+            }
+        }
+
+
+        # region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
